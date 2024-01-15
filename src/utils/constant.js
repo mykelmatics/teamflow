0248a0b9d544b78e6325fd7e52e466b91607379c
michael <Micheal.Onafusi@ng.ab-inbev.com>
2024-01-15 18:20:26 +0100

    updates

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,19 +1,23 @@
+import Business from "/src/assets/images/graph_icon.png";
+import Finance from "/src/assets/images/financial_icon.png";
+import Market from "/src/assets/images/market_icon.png";
+
 export const whatYouCandDoArr = [
   {
     id: 1,
-    imageUrl: "/src/assets/images/graph_icon.png",
+    imageUrl: Business,
     header: "Business Planning",
     text: "Our Business Plan is a written document describing a company’s core business activities.",
   },
   {
     id: 2,
-    imageUrl: "/src/assets/images/financial_icon.png",
+    imageUrl: Finance,
     header: "Financial Planning",
     text: "Our expert team sensible decision about their money, to ensure they achieve the life goals. A financial plan.",
   },
   {
     id: 3,
-    imageUrl: "/src/assets/images/market_icon.png",
+    imageUrl: Market,
     header: "Market Analysis",
     text: "A market analysis is a quantitative and qualitative assessment of a market. It looks into the size of the market.",
   },
