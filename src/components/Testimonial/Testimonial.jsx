4ca19fa790ef81f971de166a18ac0e768e03874a
michael <Micheal.Onafusi@ng.ab-inbev.com>
2024-01-15 18:07:22 +0100

    updates

--- src/components/Testimonial/Testimonial.jsx
+++ src/components/Testimonial/Testimonial.jsx
@@ -1,5 +1,11 @@
 import Slider from "react-slick";
-
+import Avatar1 from "/src/assets/images/testimonial-avatar1.png"
+import Avatar2 from "/src/assets/images/testimonial-avatar2.png"
+import Avatar3 from "/src/assets/images/testimonial-avatar3.png"
+import Avatar4 from "/src/assets/images/testimonial-avatar4.png"
+import Avatar5 from "/src/assets/images/testimonial-avatar5.png"
+import Avatar6 from "/src/assets/images/testimonial-avatar6.png"
+import TestimonialPic from "/src/assets/images/avatar (2).png"
 import QuoteIcon from "../icons/Quote";
 import "./style.css";
 
@@ -21,17 +27,17 @@ const Testimonial = () => {
           </h2>
         </div>
         <div className="testimonialContentWrapper">
-          <img src="/src/assets/images/testimonial-avatar1.png" alt="avatart" className="avatar1" />
-          <img src="/src/assets/images/testimonial-avatar2.png" alt="avatart" className="avatar2" />
-          <img src="/src/assets/images/testimonial-avatar3.png" alt="avatart" className="avatar3" />
-          <img src="/src/assets/images/testimonial-avatar4.png" alt="avatart" className="avatar4" />
-          <img src="/src/assets/images/testimonial-avatar5.png" alt="avatart" className="avatar5" />
-          <img src="/src/assets/images/testimonial-avatar6.png" alt="avatart" className="avatar6" />
+          <img src={Avatar1} alt="avatart" className="avatar1" />
+          <img src={Avatar2} alt="avatart" className="avatar2" />
+          <img src={Avatar3} alt="avatart" className="avatar3" />
+          <img src={Avatar4}alt="avatart" className="avatar4" />
+          <img src={Avatar5} alt="avatart" className="avatar5" />
+          <img src={Avatar6} alt="avatart" className="avatar6" />
           <Slider {...settings}>
             <div className="testimonialContentCard">
               <div className="testimonialContent">
                 <img
-                  src="/src/assets/images/avatar (2).png"
+                  src={TestimonialPic}
                   alt="avatar"
                   className="testimonialPic"
                 />
@@ -48,7 +54,7 @@ const Testimonial = () => {
             <div className="testimonialContentCard">
               <div className="testimonialContent">
                 <img
-                  src="/src/assets/images/avatar (2).png"
+                  src={TestimonialPic}
                   alt="avatar"
                   className="testimonialPic"
                 />
@@ -65,7 +71,7 @@ const Testimonial = () => {
             <div className="testimonialContentCard">
               <div className="testimonialContent">
                 <img
-                  src="/src/assets/images/avatar (2).png"
+                  src={TestimonialPic}
                   alt="avatar"
                   className="testimonialPic"
                 />
